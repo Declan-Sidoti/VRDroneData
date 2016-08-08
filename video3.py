79749d4bbac36d3a7d0285155bfbaa711d691752
jdhernandez88 <jdhernandez88@yahoo.com>
2016-08-08 22:57:32 +0000

    trying to get imagery working

--- video3.py
+++ video3.py
@@ -12,32 +12,42 @@ from fractions import Fraction
 from twisted.web.static import File
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory, listenWS
 import chardet
+import chardet
+import sys
+import numpy as np
+import cv2
+from pylepton import Lepton
+import io
+import time
+import picamera
 
 class Data(object):
     def __init__(self):
         print "init"
+        self.camera = picamera.PiCamera()
 
     def read(self):
         stream=io.BytesIO()
-        with picamera.PiCamera() as camera:
-            camera.resolution = (2592, 1944)
-            camera.capture (stream, format='jpeg', bayer=True)
-            return stream
-        return
+        self.camera.framerate = 10
+        self.camera.resolution = (320, 200)
+        self.camera.capture_continuous (stream, format='jpeg', bayer=True)
+        return stream.getvalue()
 
 
 class ClientProtocol(WebSocketClientProtocol):
     def __init__(self):
+        print "server connected"
         self.my_data = Data()
 
     def onOpen(self):
         #Send message to server with client identity and type
         self.sendMessage('[{"proto":{"identity":"'+str(uuid.uuid1())+'","type":"vis_image"}}]')
-        LoopingCall(self.sendBits).start(0.2)
+        LoopingCall(self.sendBits).start(2)
 
-    def sendBits(self, s):
+    def sendBits(self):
         image = base64.b64encode(self.my_data.read())
         packaged = json.dumps([{"vis_image" : image}])
+        print image
         self.sendMessage(packaged)
 
 
